e8016ac1d20e8148709f3c75054d0a47f44838d7
Lupse <lupse.ioan.victor@gmail.com>
2025-04-16 01:26:48 +0300

    Lab 7,8

---
+++ Lab8/exceptii.h
@@ -0,0 +1,8 @@
+//
+// Created by lupse on 4/14/2025.
+//
+
+#ifndef EXCEPTII_H
+#define EXCEPTII_H
+
+#endif //EXCEPTII_H
---
+++ Lab8/service_spalatorie.h
@@ -0,0 +1,8 @@
+//
+// Created by lupse on 4/15/2025.
+//
+
+#ifndef SERVICE_SPALATORIE_H
+#define SERVICE_SPALATORIE_H
+
+#endif //SERVICE_SPALATORIE_H
